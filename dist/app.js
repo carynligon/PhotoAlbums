6b62fafb3b32f8d20661934cfe701fac60eb1b8b
Caryn Ligon <carynligon@gmail.com>
2016-07-01 12:41:40 -0500

    started work on hash changes

--- dist/app.js
+++ dist/app.js
@@ -2,41 +2,37 @@ var $navBar = $('nav');
 var $mainContainer = $('#album-container');
 var $imagePreview = $('#image-preview');
 var $h2 = $('h2');
-var $albumClass = $('.album-preview');
+var $albumClass = $('.album-preview li');
 var $imgPreview = $('.img-preview');
 
 
 $(document).ready(function () {
   $h2.addClass('display-none');
   $navBar.addClass('display-none');
+  location.hash = '#home';
 
-  albums.forEach(function(album, i) {
-    var $imageHtml = $('<li class="album-preview"><img src="#" class="album-preview"/></li>');
-    var $coverLink = $imageHtml;
+  data.forEach(function(album, i) {
+    var $imageHtml = $('<a href="#>"<li><img src="#" /></li></a>');
     var $albumTitle = $('<p>Album Name</p>');
-    $coverLink.children('img').attr('src', album.cover);
-    $imagePreview.append($coverLink);
-    $coverLink.append($albumTitle);
-    $albumTitle.text(album.title);
+    $imagePreview.append($imageHtml);
+    $imageHtml.attr('href', data[i].path);
+    $imageHtml.children('img').attr('src', data[i].cover);
+    $imageHtml.append($albumTitle);
+    $albumTitle.text(data[i].title);
+    console.log(data);
   });
 });
 
-$imagePreview.on('click', function (evt) {
-  $navBar.removeClass('display-none');
-  $imagePreview.addClass('img-preview');
-
-  data.forEach(function(photo, i) {
-    if (i <= 5) {
-      $albumClass.remove('album-preview');
-      $albumClass.addClass('display-none');
-      var $imageHtml = $('<li><img src="#" class="img-preview"/></li>');
-      var $imgLink = $imageHtml;
-      var $imgTitle = $('<p>Image Name</p>');
-      $imgLink.children('img').attr('src', data.path);
-      $imagePreview.append($imgLink);
-      $imgLink.append($imgTitle);
-      $imgTitle.text(data.title);
-      console.log('hi');
-    }
-  });
+window.addEventListener('hashchange', function () {
+  var index = location.hash.slice(6) - 1;
+  var dataToRender = data[index];
+  renderContent(dataToRender);
+  console.log(dataToRender);
 });
+
+function renderContent (whatContent) {
+  data.forEach(function(album, i) {
+    var $imgArr = album.images;
+    $imagePreview.children('img').attr('src', $imgArr[whatContent].path);
+  });
+}
