0ddb165c34448b7c3dc67118bdac5f221ba16364
Caryn Ligon <carynligon@gmail.com>
2016-07-01 16:11:22 -0500

    from albums to images functioning

--- dist/data.js
+++ dist/data.js
@@ -1,228 +1,235 @@
 var data = [
   {
-    title: 'Album 1',
-    path: '#album1',
-    cover: 'images/image1.jpeg',
+    title: 'Naturey Things',
+    path: '#naturey_things',
+    id: 1,
+    cover: 'images/nature/nature1.jpeg',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        path: '#naturey_things/image/1',
+        src: 'images/nature/nature1.jpeg',
         id: 1,
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        path: '#naturey_things/image/2',
+        src: 'images/nature/nature2.jpeg',
         id: 2,
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        path: '#naturey_things/image/3',
+        src: 'images/nature/nature3.jpeg',
         id: 3,
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        path: '#naturey_things/image/4',
+        src: 'images/nature/nature4.jpeg',
         id: 4,
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        path: '#naturey_things/image/5',
+        src: 'images/nature/nature5.jpeg',
         id: 5,
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        path: '#naturey_things/image/6',
+        src: 'images/nature/nature6.jpeg',
         id: 6,
         description: 'An Image'
       }
     ]
   },
   {
-    title: 'Album 2',
-    path: '#album2',
-    cover: 'images/image2.jpeg',
+    title: 'Dogs',
+    path: '#dogs/2',
+    cover: 'images/dogs/dog1.jpeg',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        src: 'images/dogs/dog1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        src: 'images/dogs/dog2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        src: 'images/dogs/dog3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        src: 'images/dogs/dog4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        src: 'images/dogs/dog5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        src: 'images/dogs/dog6.jpeg',
         description: 'An Image'
       }
     ]
   },
   {
-    title: 'Album 3',
-    path: '#album3',
-    cover: 'images/image1.jpeg',
+    title: 'At Home',
+    path: '#at_home/3',
+    cover: 'images/home/home1.jpeg',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        src: 'images/home/home1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        src: 'images/home/home2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        src: 'images/home/home3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        src: 'images/home/home4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        src: 'images/home/home5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        src: 'images/home/home6.jpeg',
         description: 'An Image'
       }
     ]
   },
   {
     title: 'Album 4',
-    path: '#album4',
-    cover: 'images/image1.jpeg',
+    path: '#album/4',
+    cover: '#',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        src: '#',
         description: 'An Image'
       }
     ]
   },
   {
     title: 'Album 5',
-    path: '#album5',
-    cover: 'images/image5.jpeg',
+    path: '#album/5',
+    cover: '#',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        src: '#',
         description: 'An Image'
       }
     ]
   },
   {
     title: 'Album 6',
-    path: '#album6',
-    cover: 'images/image6.jpeg',
+    path: '#album/6',
+    cover: '#',
     images: [
       {
         name: 'Image 1',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        path: '#',
+        src: '#',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        path: '#',
+        src: '#',
         description: 'An Image'
       }
     ]
