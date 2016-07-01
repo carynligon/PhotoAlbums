f79cf5f4ec7a817dda804e52c4c68db93bcf40d4
Caryn Ligon <carynligon@gmail.com>
2016-07-01 12:41:40 -0500

    started work on hash changes

--- data.js
+++ data.js
@@ -1,221 +1,230 @@
-var albums = [
+var data = [
   {
     title: 'Album 1',
     path: '#album1',
     cover: 'images/image1.jpeg',
-    description: 'an album'
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        id: 1,
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        id: 2,
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        id: 3,
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        id: 4,
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        id: 5,
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        id: 6,
+        description: 'An Image'
+      }
+    ]
   },
   {
     title: 'Album 2',
     path: '#album2',
     cover: 'images/image2.jpeg',
-    description: 'an album'
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        description: 'An Image'
+      }
+    ]
   },
   {
     title: 'Album 3',
     path: '#album3',
-    cover: 'images/image3.jpeg',
-    description: 'an album'
+    cover: 'images/image1.jpeg',
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        description: 'An Image'
+      }
+    ]
   },
   {
     title: 'Album 4',
     path: '#album4',
-    cover: 'images/image4.jpeg',
-    description: 'an album'
+    cover: 'images/image1.jpeg',
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        description: 'An Image'
+      }
+    ]
   },
   {
     title: 'Album 5',
     path: '#album5',
     cover: 'images/image5.jpeg',
-    description: 'an album'
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        description: 'An Image'
+      }
+    ]
   },
   {
     title: 'Album 6',
     path: '#album6',
     cover: 'images/image6.jpeg',
-    description: 'an album'
-  },
-];
-
-var data = [
-  {
-    title: 'photo1',
-    path: 'https://images.unsplash.com/reserve/imNop2O1Rit190cSkxXv_1-7069.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4b20778d8ecf804bbe331c7fb1a9556e',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo2',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo3',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo4',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo5',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo6',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo7',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo8',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo9',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo10',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo11',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo12',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo13',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo14',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo15',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo16',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo17',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo18',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo19',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo20',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo21',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo22',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo23',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo24',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo25',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo26',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo27',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo28',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo29',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo30',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo31',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo32',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo33',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo34',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo35',
-    path: '#',
-    description: 'this is an image'
-  },
-  {
-    title: 'photo36',
-    path: '#',
-    description: 'this is an image'
+    images: [
+      {
+        name: 'Image 1',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 2',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 3',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 4',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 5',
+        path: '#',
+        description: 'An Image'
+      },
+      {
+        name: 'Image 6',
+        path: '#',
+        description: 'An Image'
+      }
+    ]
   }
 ];
