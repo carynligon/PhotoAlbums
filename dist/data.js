02af73534eb2277692b3d4b6e9f107dfa05e7d9a
Caryn Ligon <carynligon@gmail.com>
2016-07-04 15:59:12 -0500

    added rest of images and made some fixes, double click issue still happening

--- dist/data.js
+++ dist/data.js
@@ -56,31 +56,37 @@ var data = [
     images: [
       {
         name: 'Image 1',
+        path: '#dogs/2/image/1',
         src: 'images/dogs/dog1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
+        path: '#dogs/2/image/2',
         src: 'images/dogs/dog2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
+        path: '#dogs/2/image/3',
         src: 'images/dogs/dog3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
+        path: '#dogs/2/image/4',
         src: 'images/dogs/dog4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
+        path: '#dogs/2/image/5',
         src: 'images/dogs/dog5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
+        path: '#dogs/2/image/6',
         src: 'images/dogs/dog6.jpeg',
         description: 'An Image'
       }
@@ -93,143 +99,167 @@ var data = [
     images: [
       {
         name: 'Image 1',
+        path: '#at_home/3/image/1',
         src: 'images/home/home1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
+        path: '#at_home/3/image/2',
         src: 'images/home/home2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
+        path: '#at_home/3/image/3',
         src: 'images/home/home3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
+        path: '#at_home/3/image/4',
         src: 'images/home/home4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
+        path: '#at_home/3/image/5',
         src: 'images/home/home5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
+        path: '#at_home/3/image/6',
         src: 'images/home/home6.jpeg',
         description: 'An Image'
       }
     ]
   },
   {
-    title: 'Album 4',
-    path: '#album/4',
-    cover: '#',
+    title: 'Food',
+    path: '#food/4',
+    cover: 'images/food/food1.jpeg',
     images: [
       {
         name: 'Image 1',
-        src: '#',
+        path: '#food/4/image/1',
+        src: 'images/food/food1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        src: '#',
+        path: '#food/4/image/2',
+        src: 'images/food/food2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        src: '#',
+        path: '#food/4/image/3',
+        src: 'images/food/food3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        src: '#',
+        path: '#food/4/image/4',
+        src: 'images/food/food4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        src: '#',
+        path: '#food/4/image/5',
+        src: 'images/food/food5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        src: '#',
+        path: '#food/4/image/6',
+        src: 'images/food/food6.jpeg',
         description: 'An Image'
       }
     ]
   },
   {
-    title: 'Album 5',
-    path: '#album/5',
-    cover: '#',
+    title: 'Space',
+    path: '#space/5',
+    cover: 'images/space/space1.jpeg',
     images: [
       {
         name: 'Image 1',
-        src: '#',
+        path: '#space/5/image/1',
+        src: 'images/space/space1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        src: '#',
+        path: '#space/5/image/2',
+        src: 'images/space/space2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        src: '#',
+        path: '#space/5/image/3',
+        src: 'images/space/space3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        src: '#',
+        path: '#space/5/image/4',
+        src: 'images/space/space4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        src: '#',
+        path: '#space/5/image/5',
+        src: 'images/space/space5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        src: '#',
+        path: '#space/5/image/6',
+        src: 'images/space/space6.jpeg',
         description: 'An Image'
       }
     ]
   },
   {
-    title: 'Album 6',
-    path: '#album/6',
-    cover: '#',
+    title: 'Move',
+    path: '#move/6',
+    cover: 'images/move/move1.jpeg',
     images: [
       {
         name: 'Image 1',
-        src: '#',
+        path: '#move/6/image/1',
+        src: 'images/move/move1.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 2',
-        src: '#',
+        path: '#move/6/image/2',
+        src: 'images/move/move2.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 3',
-        src: '#',
+        path: '#move/6/image/3',
+        src: 'images/move/move3.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 4',
-        src: '#',
+        path: '#move/6/image/4',
+        src: 'images/move/move4.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 5',
-        src: '#',
+        path: '#move/6/image/5',
+        src: 'images/move/move5.jpeg',
         description: 'An Image'
       },
       {
         name: 'Image 6',
-        src: '#',
+        path: '#move/6/image/6',
+        src: 'images/move/move6.jpeg',
         description: 'An Image'
       }
     ]
